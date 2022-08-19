3ab0ce8261cf072e02597acfb809a04ce309b7d5
Chowewow <chowewoow@gmail.com>
2022-08-19 09:32:49 +0800

    version 1.0

---
+++ app/static/template.js
@@ -0,0 +1,45 @@
+$(document).ready(() => {
+    showDivs(slideIndex, document.getElementsByClassName("artisanSlides"));
+    showDivs(slideIndex, document.getElementsByClassName("clothesSlides"));
+    showDivs(slideIndex, document.getElementsByClassName("giftSlides"));
+    let sections = $(".sections");
+    $("#navList").children().each(function(index) {
+
+        this.id = index;
+        $(this).on("click", (item) => {
+            sections.each(function(index2) {
+                sections[index2].style.display = "none";
+            });
+            sections[this.id].style.display = "block";
+            switch (this.id) {
+                case 0:
+                    showDivs(slideIndex, document.getElementsByClassName("artisanSlides"));
+                    break;
+                case 1:
+                    showDivs(slideIndex, document.getElementsByClassName("clothesSlides"));
+                    break;
+                case 2:
+                    showDivs(slideIndex, document.getElementsByClassName("giftSlides"));
+                    break;
+                default:
+                    break;
+            }
+        });
+    });
+});
+var slideIndex = 1;
+
+function showDivs(n, x) {
+    console.log(n);
+    var i;
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
+
+function plusDivs(n, x) {
+    showDivs(slideIndex += n, x);
+}
